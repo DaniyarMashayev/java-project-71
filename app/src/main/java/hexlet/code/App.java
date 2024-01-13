aeab2efb943933b7d3fdfa708a865648f6f4d9bb
DaniyarMashayev <dmashaev@gmail.com>
2024-01-13 17:21:33 +0600

    update App.java file

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,23 +3,31 @@ package hexlet.code;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+import java.io.File;
 
-public class App {
-    public static void main(String[] args) {
-        int exitCode = new CommandLine(new genDiffCommand()).execute(args);
-        System.exit(exitCode);
-    }
-}
 @Command(name="gendiff", mixinStandardHelpOptions = true,
         description = "Compares two configuration files and shows a difference.")
-class genDiffCommand implements Runnable {
+public class App implements Runnable {
 
+    @Option(names = {"-f", "--format"}, paramLabel = "format", defaultValue = "stylish", description = "output format [default: stylish]")
+    private String format;
+
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    private File file1;
+    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    private File file2;
     @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
-    boolean help;
+    private boolean help;
     @Option(names = {"-V", "--version"}, description = "Print version information and exit.")
     boolean version;
 
     @Override
     public void run() {
+    }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
