dae657b582e1749e1eef20e5286da130a2120ed3
DaniyarMashayev <dmashaev@gmail.com>
2024-01-27 17:14:20 +0600

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,26 +4,30 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
-import java.io.File;
+
+import java.util.concurrent.Callable;
 
 @Command(name="gendiff", mixinStandardHelpOptions = true,
         description = "Compares two configuration files and shows a difference.")
-public class App implements Runnable {
+public class App implements Callable<String> {
 
     @Option(names = {"-f", "--format"}, paramLabel = "format", defaultValue = "stylish", description = "output format [default: stylish]")
     private String format;
 
     @Parameters(paramLabel = "filepath1", description = "path to first file")
-    private File file1;
+    public String file1;
     @Parameters(paramLabel = "filepath2", description = "path to second file")
-    private File file2;
+    public String file2;
     @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
     private boolean help;
     @Option(names = {"-V", "--version"}, description = "Print version information and exit.")
-    boolean version;
+    private boolean version;
 
     @Override
-    public void run() {
+    public String call() throws Exception {
+        String result = Differ.generate(file1, file2);
+        System.out.println(result);
+        return result;
     }
 
     public static void main(String[] args) {
