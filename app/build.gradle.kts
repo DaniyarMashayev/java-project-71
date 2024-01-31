3d27312923ffcf4f75808aee74b76e9c7ca45a1c
DaniyarMashayev <dmashaev@gmail.com>
2024-01-31 19:13:10 +0600

    update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("info.picocli:picocli:4.7.5")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.16.1")
 }
 
 tasks.test {
