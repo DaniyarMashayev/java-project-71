a4d71f3716965161b7851a21e552ee1143485ce9
DaniyarMashayev <dmashaev@gmail.com>
2024-01-31 19:13:41 +0600

    update DifferTest.java

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -7,7 +7,7 @@ import java.util.StringJoiner;
 
 public class DifferTest {
     @Test
-    public void testGenerate() throws Exception {
+    public void testGenerateJson() throws Exception {
         StringJoiner expected = new StringJoiner(" ", "{\n ", "}");
         expected.add("- follow: false" + "\n");
         expected.add("  host: hexlet.io" + "\n");
@@ -17,6 +17,20 @@ public class DifferTest {
         expected.add("+ verbose: true" + "\n");
         String file1 = "src/test/resources/file1.json";
         String file2 = "src/test/resources/file2.json";
+        String actual = Differ.generate(file1, file2);
+        assertEquals(expected.toString(), actual);
+    }
+    @Test
+    public void testGenerateYaml() throws Exception {
+        StringJoiner expected = new StringJoiner(" ", "{\n ", "}");
+        expected.add("- follow: false" + "\n");
+        expected.add("  host: hexlet.io" + "\n");
+        expected.add("- proxy: 123.234.53.22" + "\n");
+        expected.add("- timeout: 50" + "\n");
+        expected.add("+ timeout: 20" + "\n");
+        expected.add("+ verbose: true" + "\n");
+        String file1 = "src/test/resources/filepath1.yml";
+        String file2 = "src/test/resources/filepath2.yml";
         String actual = Differ.generate(file1, file2);
         assertEquals(expected.toString(), actual);
     }
