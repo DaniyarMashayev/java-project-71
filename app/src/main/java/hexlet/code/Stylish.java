173a37840ea588abafb714cdc0c33dcb0567bf52
DaniyarMashayev <dmashaev@gmail.com>
2024-02-05 22:01:12 +0600

    add stylish

---
+++ app/src/main/java/hexlet/code/Stylish.java
@@ -0,0 +1,29 @@
+package hexlet.code;
+
+
+import java.util.Map;
+import java.util.List;
+
+public class Stylish {
+    public static String getFormatterStylish(List<Map<String, Object>> diff) {
+        StringBuilder result = new StringBuilder();
+        result.append("{\n");
+        for (Map<String, Object> map : diff) {
+            if (map.get("status").equals("removed")) {
+                result.append("  - " + map.get("key") + ": " + map.get("oldValue") + "\n");
+            }
+            if (map.get("status").equals("added")) {
+                result.append("  + " + map.get("key") + ": " + map.get("newValue") + "\n");
+            }
+            if (map.get("status").equals("changed")) {
+                result.append("  - " + map.get("key") + ": " + map.get("oldValue") + "\n");
+                result.append("  + " + map.get("key") + ": " + map.get("newValue") + "\n");
+            }
+            if (map.get("status").equals("unchanged")) {
+                result.append("    " + map.get("key") + ": " + map.get("oldValue") + "\n");
+            }
+        }
+        result.append("}");
+        return result.toString();
+    }
+}
