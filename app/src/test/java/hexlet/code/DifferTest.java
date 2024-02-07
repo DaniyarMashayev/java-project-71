c1ae40ac746811003a94ef63125c4c341d3b4f2c
DaniyarMashayev <dmashaev@gmail.com>
2024-02-07 23:32:16 +0600

    update DifferTest.java

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 public class DifferTest {
     @Test
-    public void testGenerateJson() throws Exception {
+    public void testGenerateJsonStylish() throws Exception {
         StringBuilder expected = new StringBuilder();
         expected.append("{\n");
         expected.append("    " + "chars1: [a, b, c]" + "\n");
@@ -38,7 +38,7 @@ public class DifferTest {
         assertEquals(expected.toString(), actual);
     }
     @Test
-    public void testGenerateYaml() throws Exception {
+    public void testGenerateYamlStylish() throws Exception {
         StringBuilder expected = new StringBuilder();
         expected.append("{\n");
         expected.append("    " + "chars1: [a, b, c]" + "\n");
@@ -68,6 +68,27 @@ public class DifferTest {
         String file1 = "src/test/resources/file1-1.yml";
         String file2 = "src/test/resources/file2-2.yml";
         String actual = Differ.generate(file1, file2, "stylish");
+        assertEquals(expected.toString(), actual);
+    }
+    @Test
+    public void testGenerateJsonPlain() throws Exception {
+        StringBuilder expected = new StringBuilder();
+        expected.append("Property 'chars2' was updated. From [complex value] to false" + "\n");
+        expected.append("Property 'checked' was updated. From false to true" + "\n");
+        expected.append("Property 'default' was updated. From null to [complex value]" + "\n");
+        expected.append("Property 'id' was updated. From 45 to null" + "\n");
+        expected.append("Property 'key1' was removed" + "\n");
+        expected.append("Property 'key2' was added with value: 'value2'" + "\n");
+        expected.append("Property 'numbers2' was updated. From [complex value] to [complex value]" + "\n");
+        expected.append("Property 'numbers3' was removed" + "\n");
+        expected.append("Property 'numbers4' was added with value: [complex value]" + "\n");
+        expected.append("Property 'obj1' was added with value: [complex value]" + "\n");
+        expected.append("Property 'setting1' was updated. From 'Some value' to 'Another value'" + "\n");
+        expected.append("Property 'setting2' was updated. From 200 to 300" + "\n");
+        expected.append("Property 'setting3' was updated. From true to 'none'" + "\n");
+        String file1 = "src/test/resources/file1-1.json";
+        String file2 = "src/test/resources/file2-2.json";
+        String actual = Differ.generate(file1, file2, "plain");
         assertEquals(expected.toString(), actual);
     }
 }
