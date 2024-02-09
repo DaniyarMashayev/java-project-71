dc5e92b39cc681fd8c61cfb67f25a0d624a64bcc
DaniyarMashayev <dmashaev@gmail.com>
2024-02-09 11:03:38 +0600

    add Json.java

---
+++ app/src/main/java/hexlet/code/formatters/Json.java
@@ -0,0 +1,13 @@
+package hexlet.code.formatters;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Map;
+import java.util.List;
+
+public class Json {
+    public static String getFormatJson(List<Map<String, Object>> diff) throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(diff);
+    }
+}
