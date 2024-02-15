06ed19ff1b8dee84bf11f2b368c23864aab05d36
DaniyarMashayev <dmashaev@gmail.com>
2024-02-15 19:29:17 +0600

    update Json.java

--- app/src/main/java/hexlet/code/formatters/Json.java
+++ app/src/main/java/hexlet/code/formatters/Json.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Map;
 import java.util.List;
 
-
 public class Json {
     public static String getFormatJson(List<Map<String, Object>> diff) throws Exception {
         ObjectMapper objectMapper = new ObjectMapper();
