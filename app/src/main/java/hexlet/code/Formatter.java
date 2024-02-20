979d64e605afbe568ee120b2922b1c732478a4f3
DaniyarMashayev <dmashaev@gmail.com>
2024-02-20 18:00:49 +0600

    update Formatter.java

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -1,8 +1,8 @@
 package hexlet.code;
 
-import static hexlet.code.formatters.Stylish.getFormatStylish;
-import static hexlet.code.formatters.Plain.getFormatPlain;
-import static hexlet.code.formatters.Json.getFormatJson;
+import static hexlet.code.formatters.Stylish.formatStylish;
+import static hexlet.code.formatters.Plain.formatPlain;
+import static hexlet.code.formatters.Json.formatJson;
 
 import java.util.List;
 import java.util.Map;
@@ -10,9 +10,9 @@ import java.util.Map;
 public class Formatter {
     public static String getFormatStyle(List<Map<String, Object>> diff, String format) throws Exception {
         return switch (format) {
-            case "stylish" -> getFormatStylish(diff);
-            case "plain" -> getFormatPlain(diff);
-            case "json" -> getFormatJson(diff);
+            case "stylish" -> formatStylish(diff);
+            case "plain" -> formatPlain(diff);
+            case "json" -> formatJson(diff);
             default -> throw new RuntimeException("Format " + format + " is not correct");
         };
     }
